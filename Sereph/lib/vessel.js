bcf940d1cc11ffb7c7db7150f41d0dd6a640a722
Ryan Kotzen <Ryank@entelect.co.za>
2015-08-29 11:52:56 +0200

    moving functions directly into the return for simplicity.
    adding the ability to get and set throttle.

--- Sereph/lib/vessel.js
+++ Sereph/lib/vessel.js
@@ -1,199 +1,134 @@
 define(['./api'], function (api) {
-    function stage() {
-        api.command('f.stage');
-    }
-
-    function throttleUp() {
-        api.command('f.throttleUp');
-    }
-
-    function throttleDown() {
-        api.command('f.throttleDown');
-    }
-
-    function throttleZero() {
-        api.command('f.throttleZero');
-    }
-
-    function throttleFull() {
-        api.command('f.throttleFull');
-    }
-
-    function rcsToggle() {
-        api.toggle('f.rcs');
-    }
-
-    function rcsOn() {
-        api.toggle('f.rcs[true]');
-    }
-
-    function rcsOff() {
-        api.toggle('f.rcs[false]');
-    }
-
-    function getRcs(callback) {
-        api.command('v.rcsValue', callback);
-    }
-
-    function sasToggle() {
-        api.toggle('f.sas');
-    }
-
-    function sasOn() {
-        api.toggle('f.sas[true]');
-    }
-
-    function sasOff() {
-        api.toggle('f.sas[false]');
-    }
-
-    function getSas(callback) {
-        api.command('v.sasValue', callback);
-    }
-
-    function lightsToggle() {
-        api.toggle('f.light');
-    }
-
-    function lightsOn() {
-        api.toggle('f.light[true]');
-    }
-
-    function lightsOff() {
-        api.toggle('f.light[false]');
-    }
-
-    function getLights(callback) {
-        api.command('v.lightValue', callback);
-    }
-
-    function landingGearToggle() {
-        api.toggle('f.gear');
-    }
-
-    function landingGearDown() {
-        api.toggle('f.gear[true]');
-    }
-
-    function landingGearUp() {
-        api.toggle('f.gear[false]');
-    }
-
-    function getLandingGear(callback) {
-        api.command('v.gearValue', callback);
-    }
-
-    function toggleBreaks() {
-        api.toggle('f.brake');
-    }
-
-    function breaksOn() {
-        api.toggle('f.brake[true]');
-    }
-
-    function breaksOff() {
-        api.toggle('f.brake[false]');
-    }
-
-    function getBreaks(callback) {
-        api.command('v.brakeValue', callback);
-    }
-
-    function abort() {
-        api.toggle('f.abort');
-    }
-
-    function actionGroup1() {
-        api.toggle('f.ag1');
-    }
-
-    function actionGroup2() {
-        api.toggle('f.ag2');
-    }
-
-    function actionGroup3() {
-        api.toggle('f.ag3');
-    }
-
-    function actionGroup4() {
-        api.toggle('f.ag4');
-    }
-
-    function actionGroup5() {
-        api.toggle('f.ag5');
-    }
-
-    function actionGroup6() {
-        api.toggle('f.ag6');
-    }
-
-    function actionGroup7() {
-        api.toggle('f.ag7');
-    }
-
-    function actionGroup8() {
-        api.toggle('f.ag8');
-    }
-
-    function actionGroup9() {
-        api.toggle('f.ag9');
-    }
-
-    function actionGroup10() {
-        api.toggle('f.ag10');
-    }
-
     return {
-        stage: stage,
+        stage: function () {
+            api.command('f.stage');
+        },
         throttle: {
-            up: throttleUp,
-            down: throttleDown,
-            full: throttleFull,
-            cut: throttleZero
+            get: function (callback) {
+                api.command('f.throttle',callback);
+            },
+            set: function (value) {
+                api.command('f.setThrottle['+value+']');
+            },
+            up: function () {
+                api.command('f.throttleUp');
+            },
+            down: function () {
+                api.command('f.throttleDown');
+            },
+            full: function () {
+                api.command('f.throttleFull');
+            },
+            cut: function () {
+                api.command('f.throttleZero');
+            }
         },
         avionics: {
             rcs: {
-                toggle: rcsToggle,
-                on: rcsOn,
-                off: rcsOff,
-                get: getRcs
+                toggle: function () {
+                    api.toggle('f.rcs');
+                },
+                on: function () {
+                    api.toggle('f.rcs[true]');
+                },
+                off: function () {
+                    api.toggle('f.rcs[false]');
+                },
+                get: function (callback) {
+                    api.command('v.rcsValue', callback);
+                }
             },
             sas: {
-                toggle: sasToggle,
-                on: sasOn,
-                off: sasOff,
-                get: getSas
+                toggle: function () {
+                    api.toggle('f.sas');
+                },
+                on: function () {
+                    api.toggle('f.sas[true]');
+                },
+                off: function () {
+                    api.toggle('f.sas[false]');
+                },
+                get: function (callback) {
+                    api.command('v.sasValue', callback);
+                }
             },
             lights: {
-                toggle: lightsToggle,
-                on: lightsOn,
-                off: lightsOff,
-                get: getLights
+                toggle: function () {
+                    api.toggle('f.light');
+                },
+                on: function () {
+                    api.toggle('f.light[true]');
+                },
+                off: function () {
+                    api.toggle('f.light[false]');
+                },
+                get: function (callback) {
+                    api.command('v.lightValue', callback);
+                }
             },
             landingGear: {
-                toggle: landingGearToggle,
-                down: landingGearDown,
-                up: landingGearUp,
-                get: getLandingGear
+                toggle: function () {
+                    api.toggle('f.gear');
+                },
+                down: function () {
+                    api.toggle('f.gear[true]');
+                },
+                up: function () {
+                    api.toggle('f.gear[false]');
+                },
+                get: function (callback) {
+                    api.command('v.gearValue', callback);
+                }
             },
             breaks: {
-                toggle: toggleBreaks,
-                on: breaksOn,
-                off: breaksOff,
-                get: getBreaks
+                toggle: function () {
+                    api.toggle('f.brake');
+                },
+                on: function () {
+                    api.toggle('f.brake[true]');
+                },
+                off: function () {
+                    api.toggle('f.brake[false]');
+                },
+                get: function (callback) {
+                    api.command('v.brakeValue', callback);
+                }
             }
         },
+        abort: function () {
+            api.toggle('f.abort');
+        },
         actionGroups: {
-            1: actionGroup1,
-            2: actionGroup2,
-            3: actionGroup3,
-            4: actionGroup4,
-            5: actionGroup5,
-            6: actionGroup6,
-            7: actionGroup7,
-            8: actionGroup8,
-            9: actionGroup9,
-            10: actionGroup10
+            1: function () {
+                api.toggle('f.ag1');
+            },
+            2: function () {
+                api.toggle('f.ag2');
+            },
+            3: function () {
+                api.toggle('f.ag3');
+            },
+            4: function () {
+                api.toggle('f.ag4');
+            },
+            5: function () {
+                api.toggle('f.ag5');
+            },
+            6: function () {
+                api.toggle('f.ag6');
+            },
+            7: function () {
+                api.toggle('f.ag7');
+            },
+            8: function () {
+                api.toggle('f.ag8');
+            },
+            9: function () {
+                api.toggle('f.ag9');
+            },
+            10: function () {
+                api.toggle('f.ag10');
+            }
         }
     }
 });
