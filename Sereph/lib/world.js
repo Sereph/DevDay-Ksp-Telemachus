503a745648e229f7c7917f3488a38f6a80c71cea
Ryan Kotzen <Ryank@entelect.co.za>
2015-08-29 22:36:26 +0200

    updating code to use the built in time formatter.

--- Sereph/lib/world.js
+++ Sereph/lib/world.js
@@ -2,12 +2,12 @@ define(['./api','../bower_components/moment/moment.js'], function (api, moment) 
     return {
         getUniversalTime:function(callback){
             api.command('t.universalTime',function(response){
-                callback(formatTime(response));
+                callback(jKSPWAPI.formatters.time(response));
             });
         },
         getMissionTime:function(callback){
             api.command('v.missionTime', function(response){
-                callback(formatTime(response));
+                callback(jKSPWAPI.formatters.time(response));
             });
         },
         pause:function(){
@@ -19,11 +19,5 @@ define(['./api','../bower_components/moment/moment.js'], function (api, moment) 
         timewarp: function (factor) {
             api.command('t.timeWarp['+factor+']');
         }
-    }
-
-    function formatTime(numericalDate){
-        var timeDate = new Date(1970,0,1);
-        timeDate.setSeconds(numericalDate);
-        return moment(timeDate);
     }
 });
