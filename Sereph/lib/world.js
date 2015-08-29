e25042b58baa649d6cb149bbca9e6f9cd30c7a64
Ryan Kotzen <Ryank@entelect.co.za>
2015-08-29 19:27:02 +0200

    adding a world api.

---
+++ Sereph/lib/world.js
@@ -0,0 +1,35 @@
+define(['./api','../bower_components/moment/moment.js'], function (api, moment) {
+    return {
+        getUniversalTime:function(callback){
+            api.command('t.universalTime',function(response){
+                callback(formatTime(response));
+            });
+        },
+        getMissionTime:function(callback){
+            api.command('v.missionTime', function(response){
+                callback(formatTime(response));
+            });
+        },
+        pause:function(){
+            api.command('t.pause');
+        },
+        unpause:function(){
+            api.command('t.unpause');
+        },
+        timewarp: function (factor) {
+            api.command('t.timeWarp['+factor+']');
+        },
+        pause:function(){
+            api.command('t.pause');
+        },
+        unPause:function(){
+            api.command('t.unpause');
+        }
+    }
+
+    function formatTime(numericalDate){
+        var timeDate = new Date(1970,0,1);
+        timeDate.setSeconds(numericalDate);
+        return moment(timeDate);
+    }
+});
