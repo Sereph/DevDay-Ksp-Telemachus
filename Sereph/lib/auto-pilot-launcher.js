ff48362739079608d1cc319c9e7b60e4e76a00d6
Ryan Kotzen <Ryank@entelect.co.za>
2015-08-29 23:12:45 +0200

    adding basic ai for now

--- Sereph/lib/auto-pilot-launcher.js
+++ Sereph/lib/auto-pilot-launcher.js
@@ -1,11 +1,35 @@
-define(['./vessel','./world'], function (vessel, world) {
-    function launch(){
-        //setInterval(function(){
-        //    vessel.situation.distanceToPeriapsis(function(response){
-        //        console.log(response);
-        //    });
-        //},1000);
+define(['./vessel', './world', '../bower_components/async/dist/async.js'], function (vessel, world, async) {
+    function launch() {
+        //vessel.attitude.flyByWire.on();
+        //vessel.throttle.full();
+        //vessel.stage();
+        //limitThrottleToTerminalVelocity();
+        executeGravityTurn();
     }
+
+    function limitThrottleToTerminalVelocity() {
+        var velocityCheck = setInterval(function () {
+            vessel.situation.atmosphericDensity(function (value) {
+                console.log('Atmospheric density : ' + value);
+            });
+            vessel.situation.altitude(function (value) {
+                console.log('altitude : ' + value);
+            });
+        }, 1000);
+    }
+
+    function executeGravityTurn() {
+        var gravityTurnStep = setInterval(function () {
+            async.parallel({
+                altitude: vessel.situation.altitude,
+                velocity: vessel.situation.velocity.surface.resultant
+            }, function (error, results) {
+                console.log('results : ' + JSON.stringify(results));
+                clearInterval(gravityTurnStep);
+            });
+        }, 1000);
+    }
+
     return {
         launch: launch
     };
