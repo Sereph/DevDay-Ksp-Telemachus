094eab1b2c5879f39d8c384a0b837b9cfa930211
Ryan Kotzen <Ryank@entelect.co.za>
2015-09-17 23:10:59 +0200

    more efficiency

--- Sereph/lib/auto-pilot-launcher.js
+++ Sereph/lib/auto-pilot-launcher.js
@@ -1,19 +1,19 @@
 define(['./vessel', './world', '../bower_components/async/dist/async.js', './pid-controller', 'jquery'], function (vessel, world, async, pidController, $) {
     var _gameLoopInterval;
-    var _targetApoapsis = 85000;
-    var _maximumApoapsis = 120000;
+    var _targetApoapsis = 100000;
+    var _maximumApoapsis = 150000;
     var _minimumPeriapsis = 70000;
-    var _fairingDeployAltitude = 45000;
+    var _fairingDeployAltitude = 50000;
     var _targetPeriapsis = _targetApoapsis;
-    var _ascentThrustComplete = false;
-    var _ascentPitchComplete = false;
+    var _ascentPhaseOneComplete = false;
     var _fairingsDiscarded = false;
     var _watchingForCircularisation = false;
     var _startedCircularisationBurn = false;
+    var _timeToApoapsisToBegingCircularisationBurn = 5;
 
     function launch() {
         //setInterval(function () {
-        //    vessel.attitude.velocityOrientationAngle(function (err, result) {
+        //    vessel.attitude.roll.get(function (err, result) {
         //        console.log(result);
         //    });
         //}, 1000);
@@ -54,7 +54,7 @@ define(['./vessel', './world', '../bower_components/async/dist/async.js', './pid
     var circularisationStage = 1;
 
     function circularisationLoopUsingFirstStage(results) {
-        if (results.timeToApoapsis > 11 && _startedCircularisationBurn === false) {
+        if (results.timeToApoapsis > _timeToApoapsisToBegingCircularisationBurn && _startedCircularisationBurn === false) {
             return;
         }
         _startedCircularisationBurn = true;
@@ -76,38 +76,42 @@ define(['./vessel', './world', '../bower_components/async/dist/async.js', './pid
                 }, 1000);
             }
         } else {
-            var belowTargetPeriapsis =results.periapsis < _targetPeriapsis;
-            var aboveMinimumPeriapsis =results.periapsis > _minimumPeriapsis;
+            var belowTargetPeriapsis = results.periapsis < _targetPeriapsis;
+            var aboveMinimumPeriapsis = results.periapsis > _minimumPeriapsis;
             var aboveMaximumApoapsis = results.apoapsis > _maximumApoapsis;
             if (belowTargetPeriapsis) {
-                if(aboveMaximumApoapsis && aboveMinimumPeriapsis){
-                    vessel.throttle.cut();
-                    _watchingForCircularisation = false;
-                    stop();
+                if (aboveMaximumApoapsis && aboveMinimumPeriapsis) {
+                    return endCircularisationBurn()
                 }
                 vessel.throttle.full();
             } else {
-                vessel.throttle.cut();
-                _watchingForCircularisation = false;
-                stop();
+                endCircularisationBurn()
             }
         }
+    }
+
+    function endCircularisationBurn() {
+        vessel.throttle.cut();
+        _watchingForCircularisation = false;
+        setTimeout(function () {
+            stop();
+        }, 1000);
     }
 
     function beginMonitoringLoop() {
         var throttleControlPid = new pidController(0.01, 0.01, 0.01, 1); // k_p, k_i, k_d,
         var pitchControlPid = new pidController(0.0001, 0.01, 0.01, 1); // k_p, k_i, k_d,
         var headingControlPid = new pidController(0.0001, 0.001, 0.01, 1); // k_p, k_i, k_d,
+        var rollControlPid = new pidController(0.00001, 0.000001, 0.01, 0.35); // k_p, k_i, k_d,
         _gameLoopInterval = setInterval(function () {
             vessel.custom.getAscentInformation(function (err, results) {
-                if (!_ascentThrustComplete) {
+                if (!_ascentPhaseOneComplete) {
                     ascentThrottleLoop(throttleControlPid, results);
-                }
-                if (!_ascentPitchComplete) {
                     pitchLoop(pitchControlPid, results);
                 }
                 deployFairingsIfAllowed(results)
                 headingLoop(headingControlPid, results);
+                rollLoop(rollControlPid, results);
                 if (_watchingForCircularisation === true) {
                     circularisationLoopUsingFirstStage(results);
                 }
@@ -122,7 +126,18 @@ define(['./vessel', './world', '../bower_components/async/dist/async.js', './pid
         var targetHeading = 90;
         headingControlPid.setTarget(targetHeading);
         var correction = headingControlPid.update(results.heading);
-        vessel.attitude.pitch.set(limitPitchYawManeuvers(correction));
+        vessel.attitude.pitch.set(limitPitchRollYawManeuvers(correction));
+    }
+
+    function rollLoop(rollControlPid, results) {
+        if (results.pitch > 89) {
+            return;
+        }
+        var targetRoll = -90;
+        rollControlPid.setTarget(targetRoll);
+        var correction = rollControlPid.update(results.roll);
+        correction = correction * -1;
+        vessel.attitude.roll.set(limitPitchRollYawManeuvers(correction));
     }
 
     function ascentThrottleLoop(throttleControlPid, results) {
@@ -132,38 +147,45 @@ define(['./vessel', './world', '../bower_components/async/dist/async.js', './pid
             throttleControlPid.setTarget(targetVelocity);
         }
         if (targetVelocity === 0) {
-            vessel.throttle.cut();
-            _ascentThrustComplete = true;
-            //todo start the check for circularisation burn
+            endAscentPhaseOne();
             return;
         }
         var correction = throttleControlPid.update(results.velocity);
         vessel.throttle.set(correction);
     }
 
+    function endAscentPhaseOne() {
+        console.info('Ascent Phase Complete, waiting to circularise');
+        vessel.throttle.cut();
+        _ascentPhaseOneComplete = true;
+        vessel.attitude.flyByWire.off();
+        vessel.attitude.mechJeb.holdPrograde();
+        vessel.avionics.rcs.on();
+        _watchingForCircularisation = true;
+    }
+
     function getAscentVelocity(apoapsis, altitude) {
         if (apoapsis >= _targetApoapsis) {
             return 0;
         }
-        if (altitude < 12000) {
-            return Math.sqrt(altitude / 0.4) + 100;
+        //if (altitude < 12000) {
+        //    return Math.sqrt(altitude / 0.4) + 100;
+        //}
+        //if (altitude < 34000) {
+        //    return Math.sqrt(altitude / 0.009) - 881.5;
+        //}
+        //return 2400;
+        if (altitude < 10000) {
+            return (0.015 * altitude) + 130
         }
-        if (altitude < 35000) {
-            return Math.sqrt(altitude / 0.009) - 881.5;
-        }
-        return 2400;
+        return (Math.pow(altitude, 2) / 1100000) + 190;
     }
 
     var prograde = 'prograde';
 
     function pitchLoop(pitchControlPid, results) {
         if (results.apoapsis >= _targetApoapsis) {
-            _ascentPitchComplete = true;
-            console.info('pitch loop done')
-            vessel.attitude.flyByWire.off();
-            vessel.attitude.mechJeb.holdPrograde();
-            vessel.avionics.rcs.on();
-            _watchingForCircularisation = true;
+
             return;
         }
         var targetPitch = getIdealAscentAngle(results.altitude, results.apoapsis, results.pitch, results.velocityOrientationAngle);
@@ -176,22 +198,19 @@ define(['./vessel', './world', '../bower_components/async/dist/async.js', './pid
         }
         var correction = pitchControlPid.update(results.pitch);
         correction = correction * -1;
-        correction = limitPitchYawManeuvers(correction);
+        correction = limitPitchRollYawManeuvers(correction);
         vessel.attitude.yaw.set(correction);
     }
 
     function getIdealAscentAngle(altitude, apoapsis, pitch, velocityOrientationAngle) {
-        if (altitude < 12000) {
+        if (altitude < 10000) {
             return Math.sqrt((500000000 - Math.pow(altitude, 2)) / 61500);
         }
-        if (apoapsis < 35000 && pitch > 35) {
-            return prograde;
-        }
-        return getIdealAscentAngleForAbove35000(pitch, velocityOrientationAngle);
+        return getPitchBasedOnAngle(altitude, pitch, velocityOrientationAngle);
     }
 
-    function getIdealAscentAngleForAbove35000(pitch, velocityOrientationAngle) {
-        if (pitch <= 4) {
+    function getPitchBasedOnAngle(altitude, pitch, velocityOrientationAngle) {
+        if (pitch <= 5) {
             console.info('at target');
             return prograde;
         }
@@ -199,10 +218,16 @@ define(['./vessel', './world', '../bower_components/async/dist/async.js', './pid
             console.info(velocityOrientationAngle);
             return prograde;
         }
-        return pitch - 3;
+        if (altitude < 20000) {
+            return pitch - 2;
+        }
+        if (altitude < 30000) {
+            return pitch - 3.5;
+        }
+        return pitch - 4.5;
     }
 
-    function limitPitchYawManeuvers(correction) {
+    function limitPitchRollYawManeuvers(correction) {
         var upperBound = 0.3;
         var lowerBound = -1 * upperBound;
         if (correction < lowerBound) {
